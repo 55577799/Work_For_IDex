f772c93d91b33443451d3ad74a4f42a2ba979af7
Alena55577799 <55577799@mail.ru>
2020-04-11 11:33:15 +0300

    commit

---
+++ Variant2/open.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+  $('.close__img').click(function () {
+    $('.block').toggle("show");
+    $('.block').css('display', 'block');
+  });
+
+  $('.close__img').click(function () {
+    $('.block').remove("show");
+
+  });
+})
+			
---
+++ Vfriant1/open.js
@@ -0,0 +1,15 @@
+
+
+
+$(document).ready(function () {
+  $('.close__img').click(function () {
+    $('.block').toggle("show");
+    $('.block').css('display', 'block');
+  });
+
+  $('.close__img').click(function () {
+    $('.block').remove("show");
+
+  });
+})
+
